aa9477be5243e9bd0f5da85bd3713047ac9fd38e
grandonk666 <grandonk457@gmail.com>
2021-08-09 00:34:54 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '301fedd22050ece0479a6878da1485c6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '339e65cb-f5fa-4ecb-839f-7fb8c7e4e34d'
+};
